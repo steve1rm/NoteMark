2e85a9b5ad6d17ec479a35c36c40757f1617f3b1
steve <stevekitkat@gmail.com>
2025-06-01 15:37:07 +0100

    feat: Integrate NoteMarkTheme into App composable
    This commit replaces the default `MaterialTheme` with `NoteMarkTheme` in the main `App` composable. This ensures that the custom theming defined in `NoteMarkTheme` is applied throughout the application.

--- composeApp/src/commonMain/kotlin/me/androidbox/App.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/App.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import me.androidbox.core.presentation.designsystem.theming.NoteMarkTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -21,7 +22,7 @@ import notemark.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    NoteMarkTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
