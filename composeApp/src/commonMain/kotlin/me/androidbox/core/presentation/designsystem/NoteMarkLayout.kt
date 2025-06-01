d1be7a6a8d0a5e87b9bf16341e51138da2499a7c
steve <stevekitkat@gmail.com>
2025-06-01 15:31:27 +0100

    feat: Add core presentation design system
    This commit introduces the core presentation design system, including:
    - Color definitions for light and dark themes
    - Dimension constants for spacing
    - A base `NoteMarkLayout` composable for consistent app structure
    - Shape definitions for UI elements
    - Typography styles using the Space Grotesk font
    - The Space Grotesk font file
    
    Additionally, a basic `SplashScreen` composable is added, utilizing the new `NoteMarkLayout`.

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/core/presentation/designsystem/NoteMarkLayout.kt
@@ -0,0 +1,36 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package me.androidbox.core.presentation.designsystem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun NoteMarkLayout(
+    modifier: Modifier = Modifier.background(color = MaterialTheme.colorScheme.background),
+    toolBar: @Composable () -> Unit = {},
+    content: @Composable (paddingValue: PaddingValues) -> Unit,
+    bottomBar: @Composable () -> Unit = {}
+) {
+    Scaffold(
+        modifier = modifier.fillMaxWidth(),
+        containerColor = Color.Transparent,
+        topBar = {
+            toolBar()
+
+        },
+        content = { paddingValues ->
+            content(paddingValues)
+        },
+        bottomBar = {
+            bottomBar()
+        }
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/me/androidbox/core/presentation/designsystem/theming/Shapes.kt
@@ -0,0 +1,10 @@
+package me.androidbox.core.presentation.designsystem.theming
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(smallSpacing),
+    medium = RoundedCornerShape(mediumSpacing),
+    large = RoundedCornerShape(largeSpacing)
+)
