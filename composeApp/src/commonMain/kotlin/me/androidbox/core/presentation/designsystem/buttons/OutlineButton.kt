79f2d8c04921efbc70e20257a0054da3ff8643c1
steve <stevekitkat@gmail.com>
2025-06-01 22:23:24 +0100

    feat: Add SolidButton and OutlineButton composables
    This commit introduces two new button composables:
    
    - `SolidButton`: A filled button with a primary background color.
    - `OutlineButton`: An outlined button with a primary border and transparent background.
    
    Both buttons use the `NoteMarkTheme` and have a fixed height of `48.dp` and rounded corners of `12.dp`.
    
    Additionally, the `Typography` definition in `NoteMarkTheme` has been updated to include a `titleSmall` text style, which is now used by the new button composables.

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/core/presentation/designsystem/buttons/OutlineButton.kt
@@ -0,0 +1,53 @@
+package me.androidbox.core.presentation.designsystem.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.androidbox.core.presentation.designsystem.theming.NoteMarkTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OutlineButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    enabled: Boolean,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier.height(48.dp),
+        onClick = onClick,
+        enabled = enabled,
+        shape = RoundedCornerShape(size = 12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview
+@Composable
+fun OutlineButtonPreview() {
+    NoteMarkTheme {
+        OutlineButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Login",
+            enabled = true,
+            onClick = {}
+        )
+    }
+}
