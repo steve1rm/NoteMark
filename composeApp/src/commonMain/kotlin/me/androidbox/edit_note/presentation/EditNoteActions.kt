d6fe2223883a3f8a5fb28101b64f990d1ed351c2
steve <stevekitkat@gmail.com>
2025-06-21 17:08:52 +0100

    feat: Add save note action and integrate SaveNoteUseCase in EditNoteViewModel
    - Added `OnSaveNote` action to `EditNoteActions` for saving notes.
    - Injected `SaveNoteUseCase` into `EditNoteViewModel`.
    - Implemented save note functionality using `SaveNoteUseCase` and `Clock` for timestamp management.

--- composeApp/src/commonMain/kotlin/me/androidbox/edit_note/presentation/EditNoteActions.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/edit_note/presentation/EditNoteActions.kt
@@ -1,6 +1,9 @@
 package me.androidbox.edit_note.presentation
 
+import me.androidbox.notes.domain.model.NoteItem
+
 sealed interface EditNoteActions {
     data class OnTitleChange(val title: String) : EditNoteActions
     data class OnContentChange(val content: String) : EditNoteActions
+    data class OnSaveNote(val noteItem: NoteItem) : EditNoteActions
 }
--- composeApp/src/commonMain/kotlin/me/androidbox/edit_note/presentation/EditNoteViewModel.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/edit_note/presentation/EditNoteViewModel.kt
@@ -1,11 +1,18 @@
 package me.androidbox.edit_note.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import me.androidbox.notes.domain.model.NoteItem
+import me.androidbox.notes.domain.usecases.SaveNoteUseCase
 
-class EditNoteViewModel : ViewModel() {
+class EditNoteViewModel(
+    private val saveNoteUseCase: SaveNoteUseCase
+) : ViewModel() {
     private val _state = MutableStateFlow(EditNoteUiState())
     val state = _state.asStateFlow()
 
@@ -16,6 +23,19 @@ class EditNoteViewModel : ViewModel() {
             }
             is EditNoteActions.OnTitleChange -> {
                 _state.update { it.copy(title = action.title) }
+            }
+
+            is EditNoteActions.OnSaveNote -> {
+                viewModelScope.launch {
+                    saveNoteUseCase.execute(
+                        NoteItem(
+                            id = "random UUID",
+                            title = state.value.title,
+                            content = state.value.content,
+                            createdAt = Clock.System.now().toEpochMilliseconds(),
+                            lastEditedAt = Clock.System.now().toEpochMilliseconds()
+                        ))
+                }
             }
         }
     }
