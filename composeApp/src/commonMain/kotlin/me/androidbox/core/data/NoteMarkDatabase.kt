23a171ea03f5bb00488d016593a67a57231e71e4
Steve <steve@kitkat@gmail.com>
2025-06-25 23:21:29 +0700

    feat: Add UserEntity to NoteMarkDatabase

--- composeApp/src/commonMain/kotlin/me/androidbox/core/data/NoteMarkDatabase.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/core/data/NoteMarkDatabase.kt
@@ -2,11 +2,12 @@ package me.androidbox.core.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import me.androidbox.authentication.register.UserEntity
 import me.androidbox.notes.data.NoteMarkDao
 import me.androidbox.notes.data.models.NoteItemEntity
 
 @Database(
-    entities = [NoteItemEntity::class],
+    entities = [NoteItemEntity::class, UserEntity::class],
     version = 1)
 abstract class NoteMarkDatabase : RoomDatabase() {
     abstract fun noteMarkDao(): NoteMarkDao
