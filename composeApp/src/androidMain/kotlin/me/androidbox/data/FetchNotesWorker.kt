2a8c18d229d20915051f7c8220a1d525f8c42a71
steve <stevekitkat@gmail.com>
2025-07-15 22:52:58 +0100

    Refactor: Limit FetchNotesWorker retry attempts

--- composeApp/src/androidMain/kotlin/me/androidbox/data/FetchNotesWorker.kt
+++ composeApp/src/androidMain/kotlin/me/androidbox/data/FetchNotesWorker.kt
@@ -15,29 +15,35 @@ class FetchNotesWorker(
 ) : CoroutineWorker(context, workerParameters) {
 
     override suspend fun doWork(): Result {
-        val results = notesRepository.fetchAllNotes()
+        if(runAttemptCount >= 5) {
+            return Result.failure()
+        }
+        else {
+            val results = notesRepository.fetchAllNotes()
 
-        return when(results) {
-            is Left -> {
-                Result.success()
-            }
-            is Right -> {
-                when(results.value) {
-                    DataError.Local.DISK_FULL -> Result.failure()
-                    DataError.Local.EMPTY -> Result.failure()
-                    DataError.Local.UNKNOWN -> Result.retry()
-                    DataError.Network.BAD_REQUEST -> Result.retry()
-                    DataError.Network.REQUEST_TIMEOUT -> Result.retry()
-                    DataError.Network.METHOD_NOT_ALLOWED -> Result.failure()
-                    DataError.Network.UNAUTHORIZED -> Result.retry()
-                    DataError.Network.TOO_MANY_REQUESTS -> Result.retry()
-                    DataError.Network.NO_INTERNET -> Result.retry()
-                    DataError.Network.PAYLOAD_TOO_LARGE -> Result.failure()
-                    DataError.Network.SERVER_ERROR -> Result.retry()
-                    DataError.Network.SERIALIZATION -> Result.failure()
-                    DataError.Network.JSON_CONVERT -> Result.failure()
-                    DataError.Network.CONFLICT -> Result.failure()
-                    DataError.Network.UNKNOWN -> Result.retry()
+            return when (results) {
+                is Left -> {
+                    Result.success()
+                }
+
+                is Right -> {
+                    when (results.value) {
+                        DataError.Local.DISK_FULL -> Result.failure()
+                        DataError.Local.EMPTY -> Result.failure()
+                        DataError.Local.UNKNOWN -> Result.retry()
+                        DataError.Network.BAD_REQUEST -> Result.retry()
+                        DataError.Network.REQUEST_TIMEOUT -> Result.retry()
+                        DataError.Network.METHOD_NOT_ALLOWED -> Result.failure()
+                        DataError.Network.UNAUTHORIZED -> Result.retry()
+                        DataError.Network.TOO_MANY_REQUESTS -> Result.retry()
+                        DataError.Network.NO_INTERNET -> Result.retry()
+                        DataError.Network.PAYLOAD_TOO_LARGE -> Result.failure()
+                        DataError.Network.SERVER_ERROR -> Result.retry()
+                        DataError.Network.SERIALIZATION -> Result.failure()
+                        DataError.Network.JSON_CONVERT -> Result.failure()
+                        DataError.Network.CONFLICT -> Result.failure()
+                        DataError.Network.UNKNOWN -> Result.retry()
+                    }
                 }
             }
         }
